d2ceee32a029a9c282175666a0355e53d56bced1
Damlrca <sir.damirca@yandex.ru>
2023-04-22 17:20:14 +0300

    init

---
+++ ComputerGraphics_Raytracing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerGraphics_Raytracing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
